c744d8948695cf9bf48d0579920f1556698d5409
nikitavoitik <nikitavoitik2006@gmail.com>
2026-02-13 17:36:39 +0100

    asdasd

--- domains/group/repository.py
+++ domains/group/repository.py
@@ -5,10 +5,6 @@ from domain import Group
 
 class GroupRepository:
     def __init__(self):
-        self._groups: Dict[str, Group] = {}
-
-    def save(self, group: Group) -> None:
-        self._groups[group.id] = group
 
     def find_by_id(self, group_id: str) -> Optional[Group]:
         return self._groups.get(group_id)
